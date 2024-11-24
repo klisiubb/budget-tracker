db6bdb357d7b0786de826febf2ec7aff09d3a35d
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-11-24 19:35:26 +0100

    Added Family and Currency

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,16 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
 
-const Page = () => {
-  return <div className="flex py-16 px-16">Page</div>;
+const Page = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <div className="flex py-16 px-16 justify-center">
+      <h1 className="text-6xl font-bold text-primary">
+        Hello, {user.given_name}! 👋
+      </h1>
+    </div>
+  );
 };
 
 export default Page;
